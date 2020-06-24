fdd7ea8b4c1dd5f82abb476e89e05f5a7f66150d
Elia Borras <eliaborras@gmail.com>
2020-06-24 08:40:17 +0100

    merging

--- src/mlpipeline/experiment.py
+++ src/mlpipeline/experiment.py
@@ -10,7 +10,7 @@ class Experiment:
     """
     Experiment class that orchestrates the execution of an experiment.
     """
-
+    mlflow_uri_path = '$PROJECT_PATHS$/../../mlruns'
     def __init__(self, conf=None, data=None, pipeline=None):
         self.config = conf
         self.data = data
@@ -38,11 +38,8 @@ class Experiment:
 
     @data.setter
     def data(self, data_conf):
-
-        # TODO: Bad design, self.config must be set before self.data is set. Must be fixed
         if data_conf is None and self.config is None:
             raise ValueError("Data object must be provided if conf parameter is None")
-
         if data_conf is None and self.config is not None:
             self._data = Data(train=self.config.train,
                               test=self.config.test,
@@ -57,29 +54,33 @@ class Experiment:
 
     @pipeline.setter
     def pipeline(self, pipeline_conf):
-
-        # TODO: Bad design, self.config must be set before self.pipeline is set. Must be fixed
         if pipeline_conf is None and self.config is None:
             raise ValueError("Pipeline object must be provided if conf parameter is None")
 
-        # TODO: Bad design, self.config must be set before self.pipeline is set. Must be fixed
-        if pipeline_conf is None and self.config is not None: 
+        if pipeline_conf is None and self.config is not None:
             self._pipeline = Pipeline(self.config.transformers,
                                       self.config.vectorizer,
                                       self.config.estimators).init()
-                                      
+
         if isinstance(pipeline_conf, Pipeline):
-            self._pipeline = pipeline_conf.init()  # init the pipeline
+            self._pipeline = pipeline_conf.init()
 
-    def run(self):
+    def run(self, probabilities=False):
+        pipeline_fitted = self.pipeline.fit(self.data.train_X,
+                                            self.data.train_Y)
+        classes_ = pipeline_fitted._pipeline.steps[-1][1].classes_ if probabilities else 'outcome'
+        func = pipeline_fitted.predict_proba if probabilities else pipeline_fitted.predict
+        predicted = func(self.data.test_X)
 
-        self.pipeline.fit(self.data.train_X, self.data.train_Y)
-        predicted = self.pipeline.predict(self.data.test_X)
-
-        # Not sure a good idea
-        self.results = Metrics(self.name, self.data.test_Y, predicted)
+        self.results = Metrics(exp_name=self.name,
+                               classes=classes_,
+                               test_Y=self.data.test_Y,
+                               results=predicted)
+        return self
 
     def save_to_mlflow(self):
+        mlflow.set_tracking_uri(Experiment.mlflow_uri_path)
+        print(mlflow.get_tracking_uri())
         with mlflow.start_run(run_name=self.name):
             mlflow.log_param('file name', 'TESTTEST')
             mlflow.log_metric('accuracy', self.results.accuracy_score)
