e1084303f32ad9d7352314731a796652f3196b6b
Ernest D <ed091maf@gmail.com>
2020-06-26 22:39:44 +0100

    Word2VecVectorizer returns a list of embeddings

--- src/mlpipeline/transformers/word2vec_vectorizer.py
+++ src/mlpipeline/transformers/word2vec_vectorizer.py
@@ -5,41 +5,58 @@ import numpy as np
 
 class Word2VecVectorizer(BaseEstimator, TransformerMixin):
     """
+    Converts an array of lists with tokens into an array of lists with embeddings.
+
+    Arguments:
+    size -- vector size (dimension) returned for each token
+    iter -- number of iterations to fit all tokens to Word2Vec model
+    window -- 
+    max_embeddings -- maxinimum number of embeddings to store in each list
     """
     
-    def __init__(self, size=50, iter=50, window=10):
+    def __init__(self, size=50, iter=50, window=10, max_embeddings=None):
         self._size = size
         self._iter = iter
         self._window = window
         self._model = None
+        self._max_embeddings = max_embeddings
 
     def fit(self, X, y=None):
-        """
-
-        """
         self._model = Word2Vec(X, size=self._size, iter=self._iter, window=self._window, min_count=2)
         
         return self
 
     def transform(self, X, y=None):
+        """
+        Converts array of lists with tokens into an array of lists with embeddings.
 
-        vectors = np.empty((X.shape[0], self._size))
+        Arguments:
+        X -- array of lists with tokens
+    
+        Returns:
+        embeddings - array of lists with embeddings, each embedding having a shape (size, )
+        """
+
+        embeddings = np.empty((X.shape[0], ), dtype=object)
         i = 0
 
-        for tokens in X:
+        for list_tokens in X:
 
-            vectorized_tokens = []
+            embeddings[i] = []
+            num_tokens = 0
 
-            for t in tokens:
+            for token in list_tokens:
                 try:
-                    vectorized_tokens.append(self._model.wv[t])
+                    embeddings[i].append(self._model.wv[token])
+
+                    num_tokens += 1
+                    if self._max_embeddings is not None and num_tokens >= self._max_embeddings:
+                        break
+
                 except KeyError:
                     pass
 
-            if len(vectorized_tokens) > 0:
-                vectors[i] = np.mean(vectorized_tokens, axis=0)
-            
             i += 1
 
-        return vectors
+        return embeddings
     
