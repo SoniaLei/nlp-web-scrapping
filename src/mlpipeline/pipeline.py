a1c8d1ca3495e6c3fb90ae14336be02d4f74d46f
Elia Borras <eliaborras@gmail.com>
2020-07-09 17:29:57 +0100

    Pipelines and pipeline new designs

--- src/mlpipeline/pipeline.py
+++ src/mlpipeline/pipeline.py
@@ -1,96 +1,187 @@
+"""
+Module for creating and organizing a collection of nlp pipelines,
+with its respective parameters, cross validation and name information.
+"""
+from .gridsearch import GridSearch
+from .experiment import Experiments
 from sklearn import pipeline
 
 
-class Pipeline:
+class Pipelines:
+    """
+    Orchestrator - Creates a list of many nlp pipelines with its respective
+    names, parameters and steps/sequences of transformations with one
+    vectorizer and one estimator at the end of each pipe line.
+    """
 
-    def __init__(self, transformers, vectorizers, estimators):
+    def __init__(self, exp_name, data, transformers, vectorizers, estimators):
+        """
+        Initiates a Pipelines Object by given data, transformers, vectorizers,
+        and estimators. Note, vectorizers can be `None` if so, pipelines assumes
+        last transformer in transformers is a vectorizer. Also, transformers can
+        be `None`. If so, Pipelines assumes data already is transformed and only
+        requires vectorizers and estimators.
+
+        """
+        self.exp_name = exp_name
+        self.data = data
         self.transformers = transformers
         self.vectorizers = vectorizers
         self.estimators = estimators
-        self._pipeline = None
+        self.cv_keyword = 'cv'
+
+        self.names = self.set_pipelines_names()
+        self.sequences = self.set_pipelines_lists()
+        self.parameters = self.set_pipelines_parameters()
+        self.cvs = self.set_pipelines_cv()
+        self.predictions = {}
+        self.experiments = Experiments()
+
+    def set_pipelines_names(self):
+        """
+        Loops through transformes vectorizers and estimators names from conf file,
+        and concatenates all steps in one single `str` name used as detailed
+        experiment name. EX: 'exp_005_22_07_2020_Tokenizer_CountVectorizer_SVC'.
+        """
+        if self.vectorizers.names:
+            names = [self.exp_name + '_' + self.transformers.names + '_' + vectorizer + '_' + estimator
+                     for vectorizer in self.vectorizers.names
+                     for estimator in self.estimators.names]
+        else:
+            names = [self.exp_name + '_' + self.transformers.names + '_' + estimator
+                     for estimator in self.estimators.names]
+        return names
+
+    def set_pipelines_lists(self):
+        """
+        Creates multiple nlp pipelines by looping through each vectorizer and
+        estimator. Note if no vectorizers all pipelines will implement all the
+        transformers and one estimator from estimators. If multiple vectorizers,
+        All pipelines will have all transformers plus a combination of one
+        vectorizer and one estimator.
+        """
+        if self.vectorizers.sequence:
+            pipelines_lists = [self.transformers.sequence + [vectorizer] + [estimator]
+                               for vectorizer in self.vectorizers.sequence
+                               for estimator in self.estimators.sequence]
+
+        else:
+            pipelines_lists = [self.transformers.sequence + [estimator]
+                               for estimator in self.estimators.sequence]
+        return pipelines_lists
+
+    def set_pipelines_parameters(self):
+        """
+        Creates multiple parameter dictionaries for each pipeline created.
+        Ready to be injected in `GridSearchCV` object.
+        """
+        if self.vectorizers.parameters:
+            parameters = [{**self.transformers.parameters, **vectorizer, **estimator}
+                          for vectorizer in self.vectorizers.parameters
+                          for estimator in self.estimators.parameters]
+        else:
+            parameters = [{**self.transformers.parameters, **estimator}
+                          for estimator in self.estimators.parameters]
+        return parameters
+
+    def set_pipelines_cv(self):
+        """
+        Extracts cross validation information from estimators objects.
+        """
+        if self.vectorizers.names:
+            cvs = [cv[self.cv_keyword]
+                   for _ in self.vectorizers.names
+                   for cv in self.estimators.cvs]
+        else:
+            cvs = [cv[self.cv_keyword]
+                   for cv in self.estimators.cvs]
+        return cvs
+
+    def start_runs(self, safe_run=True):
+        """
+        Sets Experiments general characteristics and loop through all pipelines,
+        fitting and getting predictions. Adding model/pipeline predictions to
+        Experiments collection. if safe run saves metrics to `MlFlow` before
+        fitting the next pipeline. Otherwise, fits/predicts all pipelines and
+        saves all pipelines results at the end.
+        """
+        Experiments.runtime_save = safe_run
+        Experiments.test_Y = self.data.test_Y
+
+        for name, pipe_seq, params, cv in zip(self.names, self.sequences, self.parameters, self.cvs):
+            pipeline = Pipeline(name=name,
+                                steps=pipe_seq,
+                                parameters=params)
+            gridsearch = GridSearch(pipeline=pipeline.sk_pipeline,
+                                    parameters=params,
+                                    cv=cv)
+            gridsearch.fit(self.data.train_X, self.data.train_Y)
+
+            predictions = gridsearch.predict_proba(self.data.test_X)
+
+            if Experiments.classes is None:
+                Experiments.classes = gridsearch.classes
+
+            self.experiments.add_experiment(exp_name=name,
+                                            predictions=predictions)
+
+            # TODO is the below needed? Maybe for saving to csv?
+            self.predictions[name] = predictions
+
+    def collect_experiments(self):
+        return self.experiments
+
+
+class Pipeline:
+    """
+    Pipeline object storing single pipeline information,
+    providing sklearn pipeline plugin 'if needed in future'.
+    """
+    def __init__(self, *, name, steps, parameters):
+        """
+        Initializing pipeline object.
+        """
+        self.name = name
+        self.steps = steps
+        self.parameters = parameters
+        self.sk_pipeline = steps
 
     @property
-    def transformers(self):
-        return self._transformers
+    def name(self):
+        return self._name
 
-    @transformers.setter
-    def transformers(self, value):
-        if self.follows_parameter_pipeline_format('trasformers', value):
-            self._transformers = value
-
-    @property
-    def vectorizers(self):
-        return self._vectorizers
-
-    @vectorizers.setter
-    def vectorizers(self, value):
-        if self.follows_parameter_pipeline_format('vectorizers', value):
-            self._vectorizers = value
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
 
     @property
-    def estimators(self):
-        return self._estimators
+    def steps(self):
+        return self._steps
 
-    @estimators.setter
-    def estimators(self, value):
-        if self.follows_parameter_pipeline_format('estimators', value):
-            self._estimators = value
+    @steps.setter
+    def steps(self, values):
+        self._steps = values
 
-    def follows_parameter_pipeline_format(self, param_name, parameter):
-        """Validates parameters have the type of objects expected
-        before adding those to the pipeline"""
-        if not isinstance(parameter, list):
-            raise TypeError(f"Parameter {param_name} passed in pipeline "
-                            f"must be of type list found {type(parameter)} instead.")
-        for sub_param in parameter:
-            if not isinstance(sub_param, tuple):
-                raise TypeError(f"For parameter {param_name} passed in pipeline"
-                                f"{sub_param} must be of type Tuple found type"
-                                f"{type(sub_param)} instead")
-            if not isinstance(sub_param[0], str):
-                raise TypeError(f"For parameter {param_name} passed in pipeline"
-                                f"{sub_param} tuple first value must be of type "
-                                f"str found {type(sub_param[0])} instead")
-            if not isinstance(sub_param[1], object):
-                raise TypeError(f"For parameter {param_name} passed in pipeline"
-                                f"{sub_param} tuple first value must be of type "
-                                f"instance object found {type(sub_param[1])} instead")
-            return True
+    @property
+    def parameters(self):
+        return self._parameters
 
-    def init(self):
-        """Puts all transformers one vectorizer for now and first estimator
-        This will change once implementing grid search approach"""
+    @parameters.setter
+    def parameters(self, values):
+        self._parameters = values
 
-        steps = []
-        for t in self.transformers:
-            steps.append(t)
-        
-        steps.append(self.vectorizers[0]) # getting first vec being passed
-        steps.append(self.estimators[0]) # getting first est being passed
+    @property
+    def sk_pipeline(self):
+        """
+        Returns a sklearn pipeline object.
+        """
+        return self._sk_pipeline
 
-        self._pipeline = pipeline.Pipeline(steps=steps)
-
-        return self
-
-    def fit(self, X=None, y=None):
-        if self._pipeline is None:
-            raise ValueError("Pipeline hasn't been created. Use method init() to create it first.")
-
-        self._pipeline.fit(X, y)
-        return self
-
-    def fit_transform(self, X=None, y=None):
-        self._pipeline.fit_transform(X, y)
-        return self
-
-    def predict(self, X=None):
-        if self._pipeline is None:
-            raise ValueError("Pipeline hasn't been created. Use method init() to create it first.")
-
-        return self._pipeline.predict(X)
-
-    def predict_proba(self, X=None):
-        if self._pipeline is None:
-            raise ValueError("Pipeline hasn't been created. Use method init() to create it first.")
-
-        return self._pipeline.predict_proba(X)
+    @sk_pipeline.setter
+    def sk_pipeline(self, steps):
+        """
+        Initializes a pipeline from sklearn with steps from
+        Pipelines combinations.
+        """
+        self._sk_pipeline = pipeline.Pipeline(steps=steps)
