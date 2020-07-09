b6115d6c9d5e0abf83465da60da89f6e152b8a13
Elia Borras <eliaborras@gmail.com>
2020-07-09 17:24:57 +0100

    todo consolidate imports

---
+++ src/mlpipeline/__init__.py
@@ -0,0 +1,25 @@
+# TODO fix imports
+# from .utiles import read_config_file, validate_path
+# from .experiment import Experiments, Experiment
+# from .factory import ObjectFactory
+# from .gridsearch import GridSearch
+# from .metrics import Metrics
+# from .pipeline import Pipelines, Pipeline
+# from .context import Context
+# from .components import Data, Transformers, Vectorizers, Estimators
+#
+# all = ['read_config_file',
+#        'Context',
+#        'Transformers',
+#        'Vectorizers',
+#        'Estimators',
+#        'Data',
+#        'Experiments',
+#        'Experiment',
+#        'ObjectFactory',
+#        'GridSearch',
+#        'Metrics',
+#        'MLFlow',
+#        'Pipelines',
+#        'Pipeline',
+#        ]
