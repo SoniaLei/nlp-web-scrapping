dee61e5be5d5513e41adc3fb6a232abe88b47da4
Elia Borras <eliaborras@gmail.com>
2020-07-09 17:27:41 +0100

    Experiment/s obj

--- src/mlpipeline/experiment.py
+++ src/mlpipeline/experiment.py
@@ -1,107 +1,117 @@
-"""Experiment module to create, run and store experiments' artifacts."""
-from .pipeline import Pipeline
-from .context import Context
+"""
+Experiment module to create, run and store experiments' artifacts.
+"""
+import numpy as np
+from itertools import combinations
 from .metrics import Metrics
-from .data import Data
-from .mlflow import MLFlow
-import os
+import mlflow
+import tempfile
+
 
 class Experiment:
     """
     Experiment class that orchestrates the execution of an experiment.
     """
-    
     mlflow_uri_path = '$PROJECT_PATHS$/../../mlruns'
-    
-    def __init__(self, conf=None, data=None, pipeline=None):
-        self.config = conf
-        self.data = data
-        self.pipeline = pipeline
-        self.results = None
 
-    @property
-    def config(self):
-        return self._conf
-
-    @config.setter
-    def config(self, conf):
-        if isinstance(conf, Context) or conf is None:
-            self._conf = conf
-        else:
-            raise ValueError("Configuration must be a Context object.")
+    def __init__(self, name, predictions, classes, test_Y):
+        self.name = name
+        self.predictions = predictions
+        self.metrics = Metrics(exp_name=name,
+                               predictions=predictions,
+                               classes=classes,
+                               test_Y=test_Y)
 
     @property
     def name(self):
-        return self.config.exp_name
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, tuple):
+            value = "__".join(value)
+        self._name = value
 
     @property
-    def data(self):
-        return self._data
+    def predictions(self):
+        return self._predictions
 
-    @data.setter
-    def data(self, data_conf):
-        if data_conf is None and self.config is None:
-            raise ValueError("Data object must be provided if conf parameter is None")
-        if data_conf is None and self.config is not None:
-            self._data = Data(train=self.config.train,
-                              test=self.config.test,
-                              target=self.config.target,
-                              features=self.config.features)
-        if isinstance(data_conf, Data):
-            self._data = data_conf
-
-    @property
-    def pipeline(self):
-        return self._pipeline
-
-    @pipeline.setter
-    def pipeline(self, pipeline_conf):
-        if pipeline_conf is None and self.config is None:
-            raise ValueError("Pipeline object must be provided if conf parameter is None")
-
-        if pipeline_conf is None and self.config is not None:
-            self._pipeline = Pipeline(self.config.transformers,
-                                      self.config.vectorizers,
-                                      self.config.estimators).init()
-
-        if isinstance(pipeline_conf, Pipeline):
-            self._pipeline = pipeline_conf.init()
-
-    def run(self, probabilities=False):
-        pipeline_fitted = self.pipeline.fit(self.data.train_X,
-                                            self.data.train_Y)
-        classes_ = pipeline_fitted._pipeline.steps[-1][1].classes_ if probabilities else 'outcome'
-        
-        func = pipeline_fitted.predict_proba if probabilities else pipeline_fitted.predict
-        
-        predicted = func(self.data.test_X)
-
-        self.results = Metrics(exp_name=self.name,
-                               classes=classes_,
-                               test_Y=self.data.test_Y,
-                               results=predicted)
-        
-        return self
+    @predictions.setter
+    def predictions(self, value):
+        self._predictions = value
 
     def save_to_mlflow(self):
-        
-        mlflow = MLFlow(experiment_name=self.name,
-                        tracking_uri=self.mlflow_uri_path)
-        
-        estimator = str(self.pipeline.estimators).split(",")[1][:-2]
-        vectoriser = str(self.pipeline.vectorizers).split("'")[2][2:-2]
-        transformer = str(self.pipeline.transformers).split("'")[2][2:-2]
-        
-        params_dict = {"Estimator": estimator,
-                       "Vectoriser": vectoriser,
-                       "Transformer": transformer}
-        
-        accuracy = self.results.accuracy_score
-        
-        metrics_dict = {"Accuracy": accuracy}
-                       #"F1": self.results.f1_score}
-        
-        mlflow.Logging(params_dictionary=params_dict,
-                      metrics_dictionary=metrics_dict)
-        
-        print("Parameters, Metrics and Artifacts logged!")
+        mlflow.set_tracking_uri(Experiment.mlflow_uri_path)
+
+        with mlflow.start_run(run_name=self.name):
+            mlflow.log_param('Exp name', self.metrics.exp_name)
+            mlflow.log_param('f1 score', self.metrics.f1_score())
+            mlflow.log_metric('accuracy', self.metrics.accuracy_score)
+
+            for plot in [self.metrics.plot_confusion_matrix(), self.metrics.plot_model_roc()]:
+
+                with tempfile.NamedTemporaryFile(suffix=".png", prefix="Plot_", delete=False) as tmpfile:
+                    plot.savefig(tmpfile, format='png')
+                    tmpfile.seek(0)
+                    mlflow.log_artifact(tmpfile.name)
+
+            with tempfile.NamedTemporaryFile(suffix=".png", prefix="Plot_", delete=False) as tmpfile:
+                self.metrics.plot_classes_roc().savefig(tmpfile, format='png')
+                tmpfile.seek(0)
+                mlflow.log_artifact(tmpfile.name)
+
+
+
+class Experiments:
+    """
+    Experiments class that orchestrates the execution of multiple experiment,
+    including combination of experiments.
+    """
+    runtime_save = True
+    test_Y = None
+    classes = None
+    collection = {}
+
+    def add_experiment(self, exp_name, predictions):
+        experiment = Experiment(name=exp_name,
+                                predictions=predictions,
+                                classes=Experiments.classes,
+                                test_Y=Experiments.test_Y)
+        if self.runtime_save:
+            experiment.save_to_mlflow()
+
+        self.collection[exp_name] = experiment
+
+    def save_exp_combinations(self, experiments_dict):
+
+        for name, predictions in experiments_dict.items():
+            self.add_experiment(exp_name=name,
+                                predictions=predictions)
+
+
+    def compute_exp_combinations(self):
+        print("Computing exp permutations . . . ")
+        combined_models = {}
+        num_models = list(range(1, len(self.collection.keys())))
+        combined_models_names = []
+        print("IN Experiments.collection already : ", Experiments.collection)
+
+        for num_model in num_models:
+            combination = combinations(self.collection.keys(), num_model + 1)
+            combined_models_names.extend(list(combination))
+
+        for num, combined_model in enumerate(combined_models_names):
+            print(f"Computing {num} model . . . ", combined_model)
+            num_models = len(combined_model)
+            weight = 1 / num_models
+
+            proba = np.zeros((len(self.test_Y), len(self.classes)))
+
+            for model in combined_model:
+                print('model ', model)
+
+                prob_weighted = self.collection[model].predictions * weight
+                proba += prob_weighted
+
+            combined_models[combined_model] = proba
+        return combined_models
