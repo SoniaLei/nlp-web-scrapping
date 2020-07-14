1cc10b4cbf33f29e0c6b63822fb570f9640526a9
Elia Borras <eliaborras@gmail.com>
2020-07-14 07:28:11 +0100

    imports in __init__

--- src/mlpipeline/__init__.py
+++ src/mlpipeline/__init__.py
@@ -1,25 +1,7 @@
-# TODO fix imports
-# from .utiles import read_config_file, validate_path
-# from .experiment import Experiments, Experiment
-# from .factory import ObjectFactory
-# from .gridsearch import GridSearch
-# from .metrics import Metrics
-# from .pipeline import Pipelines, Pipeline
-# from .context import Context
-# from .components import Data, Transformers, Vectorizers, Estimators
-#
-# all = ['read_config_file',
-#        'Context',
-#        'Transformers',
-#        'Vectorizers',
-#        'Estimators',
-#        'Data',
-#        'Experiments',
-#        'Experiment',
-#        'ObjectFactory',
-#        'GridSearch',
-#        'Metrics',
-#        'MLFlow',
-#        'Pipelines',
-#        'Pipeline',
-#        ]
+from .context import Context
+from .pipeline import Pipelines
+
+__all__ = [
+           'Context',
+           'Pipelines',
+]
