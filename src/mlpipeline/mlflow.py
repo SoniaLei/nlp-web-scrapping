32c4a1554a8ea472b7f281eea16e2ac46170c9fa
Elia Borras <eliaborras@gmail.com>
2020-07-15 15:22:48 +0100

    integrating mlflow

--- src/mlpipeline/mlflow.py
+++ src/mlpipeline/mlflow.py
@@ -1,155 +1,147 @@
-# Imports
 import mlflow
 import mlflow.sklearn
+from tempfile import NamedTemporaryFile
+import os
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
-class MLFlow:
-    
-# Constructor
-    
-    def __init__(self, experiment_name = None, tracking_uri = None):
+class MlFlow:
+
+    exp_folder = 'NLP'
+
+    def __init__(self, experiment_name=None, tracking_uri=None):
         self.experiment_name = experiment_name
         self.tracking_uri = tracking_uri
-        
+
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-        
+
 # Getters and Setters
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
-# experiment_name
-    # The name of the experiment the results will be saved in for the MLFlow UI
 
     @property
     def experiment_name(self):
         return self._experiment_name
-    
-    @experiment_name.setter
-    def experiment_name(self, experiment_name):
-        self._experiment_name = experiment_name
-        
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
-# tracking_uri
-    # The filepath the results folder will be saved in
-    
+    @experiment_name.setter
+    def experiment_name(self, exp_name):
+        """The name of the experiment the results will be saved in for the MLFlow UI"""
+        if not isinstance(exp_name, str):
+            raise TypeError("Input needs to be a String")
+        self._experiment_name = exp_name
+
     @property
     def tracking_uri(self):
         return self._tracking_uri
-    
+
     @tracking_uri.setter
-    def tracking_uri(self, tracking_uri):
-        
-        uri = ""
-        
-        if tracking_uri == None:
-            
-            uri = None
-        
-        else:
-            
-            filepath_list = tracking_uri.split("/")
-            
-            # The uri needs to end with the mlruns folder
-            if filepath_list[-1] != "mlruns":
-            
-                tracking_uri += "/mlruns"
-                
-                # To set, uri needs "file:///" at the start
-                uri = "file:///" + str(tracking_uri)
-                
-        self._tracking_uri = tracking_uri
-        
-        # Set mlflow tracking_uri
-        mlflow.set_tracking_uri(self._tracking_uri)
+    def tracking_uri(self, uri):
+        """filepath the results folder will be saved in"""
+
+        if not isinstance(uri, str):
+            raise TypeError("Input needs to be a String")
+
+        print("Tracking_uri: " + str(uri))
+        filepath_list = uri.split("/")
+
+        # The uri needs to end with the mlruns folder
+        if filepath_list[-1] != "mlruns":
+            uri += "/mlruns"
+
+        # To set, uri needs "file:///" at the start
+        uri = "file:/" + str(uri)
+
+        self._tracking_uri = uri
+
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-    
+
 # Functions
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-    """
-    Get experiment_id using self.experiment_name
-    """
 
-    def Experiment_Id(self):
-        
-        experiment_name = self.experiment_name
-        
-        if experiment_name == None:
-            
+    def set_experiment_id(self, name):
+        """
+        Get experiment_id using self.exp_folder
+        """
+        if name is None:
             experiment_id = None
-            
+
         else:
-        
-            # Get experiment_id from already created experiment
+            # Get experiment_id from already created experiment name -> 'NLP'
             try:
-                experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
+                mlflow.get_experiment_by_name(name).experiment_id
                 # Inputs:
                     # name = the experiment's name
                 # Returns: Experiment object
                     # .experiment_id = Integer - value wanted from object
 
             # Create new experiment and get experiment_id
-            except AttributeError: # Error: if experiment doesn't exist ...
-                experiment_id = mlflow.create_experiment(experiment_name)
+            except AttributeError:  # Error: if experiment doesn't exist ...
+                mlflow.create_experiment(name)
                 # Inputs:
                     # name = the experiment's name (unique)
                     # artifact_location (optional) = location to store run artifacts
                 # Returns: integer Id of the experiment
-                
-                print("New experiment created in tracking_uri filepath:\n" + str(self.tracking_uri))
-            
-        return experiment_id
-    
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-    """
-    Logging to mlflow():
-        Inputs:
-            Parameters - Dictionary
-            Metrics    - Dictionary
-            Artifacts  - List (Optional)
-    """
 
-    def Logging(self, params_dictionary, metrics_dictionary, artifact_filepaths = []):
-                
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+
+    def log_artifact_tempfile(self, name, artifact):
+        """
+        Using TemporaryFiles to log figures/artifacts
+            Input:
+                Artifact - figure (usually matplotlib)
+        """
+        tmpfile = NamedTemporaryFile(
+            delete=False,
+            prefix=name + '_',
+            suffix=".png"
+        )
+
+        artifact.savefig(tmpfile)
+
+        tmpfile.close()
+
+        mlflow.log_artifact(tmpfile.name)
+
+        os.remove(tmpfile.name)
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+
+
+    def logging(self, params_dictionary, metrics_dictionary=None, artifact_objs=None,
+                is_aggregated=False):
+        """
+        Logging to mlflow():
+            Inputs:
+                Parameters - Dictionary
+                Metrics    - Dictionary
+                Artifacts  - Dictionary
+        """
+        metrics_dictionary = metrics_dictionary or {}
+        artifact_objs = artifact_objs or {}
+
+        #  connects to a tracking URI our case is Local
+        mlflow.set_tracking_uri(self.tracking_uri)
         # Getting experiment_id
-        experiment_id = self.Experiment_Id()
-        
+        # Does create new folder for each new experiment_id
+        self.set_experiment_id(self.exp_folder)
+
         # Start mlflow():
-        with mlflow.start_run(experiment_id = experiment_id):
-            
+        with mlflow.start_run(run_name=self.experiment_name):
+
+            # Tags
+            mlflow.set_tag('combined_models', is_aggregated)
+
             # Parameters
             for key in params_dictionary:
-                
                 mlflow.log_param(key, params_dictionary[key])
-            
+
             # Metrics
             for key in metrics_dictionary:
-                
                 mlflow.log_metric(key, metrics_dictionary[key])
-                
-            # Figures / Artifacts            
-            if isinstance(artifact_filepaths, list): 
-                for filepath in artifact_filepaths:
 
-                    try:
-
-                        mlflow.log_artifact(filepath)
-
-                    except FileNotFoundError:
-
-                        print("Unable to upload artifact")
-                        print("-- File not found --")
-
-            elif isinstance(artifact_filepaths, str):
-                
-                try:
-                    
-                    mlflow.log_artifact(artifact_filepaths)
-                    
-                except FileNotFoundError:
-                    
-                    print("Unable to upload artifact")
-                    print("-- File not found --")
+            # Figures / Artifacts
+            for name, artifact in artifact_objs.items():
+                self.log_artifact_tempfile(name, artifact)
