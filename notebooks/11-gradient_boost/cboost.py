4ca712b39b43a53a0fefb87de6817c85744b43f9
Nathanael Hitch <nat.hitch@btinternet.com>
2020-07-27 11:16:33 +0100

    Notebooks and scripts on catboost and lgbm gradient boost models

---
+++ notebooks/11-gradient_boost/cboost.py
@@ -0,0 +1,34 @@
+from catboost import CatBoostClassifier
+from sklearn.preprocessing import LabelEncoder
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+
+from NLP_Functions import text_cleaner, readCSVs_URL
+
+X_train, Y_train, X_test, Y_test = readCSVs_URL(
+    url_train="https://raw.githubusercontent.com/SoniaLei/nlp-web-scrapping/development/data/raw/tweets-train.csv",
+    url_test="https://raw.githubusercontent.com/SoniaLei/nlp-web-scrapping/development/data/raw/tweets-test.csv"
+)
+
+encoder_lb = LabelEncoder()
+
+Y_train = encoder_lb.fit_transform(Y_train)
+Y_test = encoder_lb.fit_transform(Y_test)
+
+tfidf_vector = TfidfVectorizer(tokenizer = text_cleaner)
+
+cbc = CatBoostClassifier(n_estimators=100)
+
+pipe = Pipeline([("vectorizer", tfidf_vector),
+                ("classifier", cbc)])
+
+pipe.fit(X_train, Y_train)
+
+predicted = pipe.predict(X_test)
+
+print("Accuracy:",metrics.accuracy_score(Y_test, predicted))
+print("Precision:",metrics.precision_score(Y_test, predicted, average="macro"))
+print("Recall:",metrics.recall_score(Y_test, predicted, average="macro"))
